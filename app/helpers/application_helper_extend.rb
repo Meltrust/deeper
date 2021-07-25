e18932372ab7dc4e789016b1d00927c2e2177079
meltrust <original.mtapia@outlook.com>
2021-07-25 01:41:55 -0500

    Remove variable assign from vieww

--- app/helpers/application_helper_extend.rb
+++ app/helpers/application_helper_extend.rb
@@ -26,4 +26,23 @@ module ApplicationHelperExtend
       }
     }
   end
+
+  def users_meta_tags(user)
+    set_meta_tags title: user.fullname,
+                  description: "#{user.fullname.split[0]} is on Deeper! Share your daily wisdom",
+                  twitter: {
+                    card: 'summary',
+                    title: user.fullname,
+                    description: "#{user.fullname.split[0]} is on Deeper! Share your daily wisdom",
+                    image: Cloudinary::Utils.cloudinary_url(user.photo.key, gravity: 'face', crop: 'fill',
+                                                                            height: '1000', width: '1200')
+                  },
+                  og: {
+                    title: user.fullname,
+                    url: "https://deeper-app.herokuapp.com#{url_for(user_path(user))}",
+                    description: "#{user.fullname.split[0]} is on Deeper! Share your daily wisdom",
+                    image: Cloudinary::Utils.cloudinary_url(user.photo.key, gravity: 'face', crop: 'fill',
+                                                                            height: '1000', width: '1200')
+                  }
+  end
 end
